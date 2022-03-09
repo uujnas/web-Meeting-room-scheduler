2a2dbc77d25dc2310ef3eea4e8614aa377c47c05
Bikash Shrestha <bikashshrestha@bajratechnologies.com>
2022-03-09 18:24:02 +0545

    feat(room): create rooms controller

---
+++ app/controllers/rooms_controller.rb
@@ -0,0 +1,57 @@
+# Rooms Controller
+class RoomsController < ApplicationController
+  before_action :set_room, only: %i[show edit update destroy]
+
+  # GET /rooms
+  def index
+    @rooms = Room.all
+  end
+
+  # GET /rooms/1
+  def show; end
+
+  # GET /rooms/new
+  def new
+    @room = Room.new
+  end
+
+  # GET /rooms/1/edit
+  def edit; end
+
+  # POST /rooms
+  def create
+    @room = Room.new(room_params)
+
+    if @room.save
+      redirect_to room_url(@room), notice: 'Room was successfully created.'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH/PUT /rooms/1
+  def update
+    if @room.update(room_params)
+      redirect_to room_url(@room), notice: 'Room was successfully updated.'
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /rooms/1
+  def destroy
+    @room.destroy
+
+    redirect_to rooms_url, notice: 'Room was successfully destroyed.'
+  end
+
+  private
+
+  def set_room
+    @room = Room.find(params[:id])
+  end
+
+  def room_params
+    params.require(:room).permit(:description, :user_id)
+  end
+end
