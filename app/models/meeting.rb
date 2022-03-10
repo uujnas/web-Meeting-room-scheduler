a8356469dceae78f3bcff38ce3ae5db80906b979
Bikash Shrestha <bikashshrestha@bajratechnologies.com>
2022-03-10 14:53:00 +0545

    feat: validate meeting and set presence

--- app/models/meeting.rb
+++ app/models/meeting.rb
@@ -1,5 +1,25 @@
 # frozen_string_literal: true
+
 class Meeting < ApplicationRecord
   belongs_to :room
   belongs_to :user
+
+  validates :subject, presence: true, length: { minimum: 5 }
+
+  validates_presence_of :date, :start_time, :end_time
+
+  # after_initialize do
+  #   start_time = Time.parse(self.start_time.to_s)
+  #   self.start_time = start_time.strftime("%H:%M %p")
+  #   end_time = Time.parse(self.end_time.to_s)
+  #   self.end_time = end_time.strftime("%H:%M %p")
+  # end
+
+  def time_format(time)
+    Time.parse(time.to_s).strftime("%H:%M %p")
+  end
+
+  before_save do
+    self.subject = subject.titleize
+  end
 end
