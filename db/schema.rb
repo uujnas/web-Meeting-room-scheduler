b6ceda3c747b0da9a601a796e18e179c0b75b89f
Bikash Shrestha <bikashshrestha@bajratechnologies.com>
2022-03-10 12:49:04 +0545

    feat(meeting): add members as an array

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_03_09_112541) do
+ActiveRecord::Schema[7.0].define(version: 2022_03_10_065830) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "meetings", force: :cascade do |t|
+    t.string "subject"
+    t.bigint "room_id", null: false
+    t.date "date"
+    t.time "start_time"
+    t.time "end_time"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.text "members", default: [], array: true
+    t.index ["room_id"], name: "index_meetings_on_room_id"
+    t.index ["user_id"], name: "index_meetings_on_user_id"
+  end
 
   create_table "rooms", force: :cascade do |t|
     t.string "description"
@@ -41,5 +55,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_03_09_112541) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "meetings", "rooms"
+  add_foreign_key "meetings", "users"
   add_foreign_key "rooms", "users"
 end
