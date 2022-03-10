0c684d7d7ebe20bb3d4ec65f6306cb8da2c2ec68
Sanju Basnet <sanjubasnet@bajratechnologies.com>
2022-03-10 13:19:15 +0545

    fix: rescue_from CanCan::AccessDenied

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,4 +9,8 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :email, :contact, :address, :role])
     devise_parameter_sanitizer.permit(:account_update, keys: [:username, :email, :contact, :address, :role])
   end
+
+  rescue_from CanCan::AccessDenied do |exception|
+    redirect_to root_url, alert: exception.message
+  end
 end
