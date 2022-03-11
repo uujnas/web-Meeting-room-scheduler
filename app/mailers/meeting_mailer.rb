b7f6d6b22fbfff336fabc1d8d19026c6b245d1d4
Bikash Shrestha <bikashshrestha@bajratechnologies.com>
2022-03-11 20:26:08 +0545

    feat: send mail to member

--- app/mailers/meeting_mailer.rb
+++ app/mailers/meeting_mailer.rb
@@ -1,2 +1,12 @@
+# frozen_string_literal: true
+
+# MeetingMailer to send meeting details to members
 class MeetingMailer < ApplicationMailer
+  def send_mail
+    @meeting = params[:meeting]
+    @member = User.find_by_email(params[:member])
+    @url = ENV["MEETING_URL"]
+
+    mail(to: @member.email, subject: @meeting.subject)
+  end
 end
