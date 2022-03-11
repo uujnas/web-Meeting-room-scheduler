afc4e0bf6ea8aeaf373fa521912d01ebf979ce0d
Bikash Shrestha <bikashshrestha@bajratechnologies.com>
2022-03-11 21:37:10 +0545

    chore: add send mails path

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@
 
 Rails.application.routes.draw do
   get "/send-mails", to: "send_mails#index", as: "send_mails"
+  post "/send-mails", to: "meetings#send_mails", as: "send_mail"
   resources :meetings
   devise_for :users, controllers: {
     confirmations: "confirmations"
