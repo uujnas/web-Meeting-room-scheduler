da8dcebf727efba2296c4d769cfbc1d0f21c350d
Bikash Shrestha <bikashshrestha@bajratechnologies.com>
2022-03-11 20:37:46 +0545

    chore: add routes for send mail

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  get "/send-mails", to: "send_mails#index", as: "send_mails"
   resources :meetings
   devise_for :users, controllers: {
     confirmations: "confirmations"
