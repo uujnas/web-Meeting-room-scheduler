0ffc33c165596c9ea04d2fabab19201a98c8c9fb
Sanju Basnet <sanjubasnet@bajratechnologies.com>
2022-03-11 12:46:26 +0545

    fix: inherit dashboard design instead of application

--- app/controllers/meetings_controller.rb
+++ app/controllers/meetings_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 # Meeting Controller
-class MeetingsController < ApplicationController
+class MeetingsController < DashboardsController
   before_action :authenticate_user!
   before_action :set_meeting, only: %i[show edit update destroy]
 
