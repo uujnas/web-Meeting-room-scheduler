2520f17eef894b3fa117fc06331f81daf0222acb
Bikash Shrestha <bikashshrestha@bajratechnologies.com>
2022-03-12 22:45:15 +0545

    fix: fix past date/time selection

--- app/models/meeting.rb
+++ app/models/meeting.rb
@@ -26,28 +26,23 @@ class Meeting < ApplicationRecord
   end
 
   def start_and_end_time
-    if !start_time.blank? && !end_time.blank? && Date.parse(date.to_s) < Date.parse(Time.now.to_s) && (Time.parse(start_time.to_s) < Time.now)
+    if Time.parse(date.to_s) < Date.parse(Time.now.to_s) && (Time.parse(start_time.to_s) < Time.now)
       errors.add(:start_time, "can't be in the past!")
-      # puts "&&&&&&", end_time < start_time
-      errors.add(:start_time, "cant' start before end time!") if end_time < start_time
-      errors.add(:end_time, "start time and end time can't be same") if start_time == end_time
     end
+
+    errors.add(:start_time, "cant' start before end time!") if end_time < start_time
+    errors.add(:end_time, "start time and end time can't be same") if start_time == end_time
   end
 
   def meeting_start_date
-    errors.add(:date, "Can't be in the past!") if !date.blank? && (Date.parse(date.to_s) < Date.parse(Time.now.to_s))
+    errors.add(:date, "Can't be in the past!") if Date.parse(date.to_s) < Date.parse(Time.now.to_s)
   end
 
   def schedule
     data = Meeting.select(:start_time, :end_time, :id).where(date: date).where.not(id: id).where(room_id: room_id)
-    # puts "#{date} = #{start_time}= #{end_time}"
+
     data.each do |meeting|
-      # puts meeting.start_time, meeting.end_time, "**" * 20
-
-      # puts meeting.start_time.hour, start_time.hour, "###" * 10
-
       errors.add(:start_time, "Meeting is scheduled before another meeting is over!") if start_time < meeting.end_time
-
       if meeting.start_time.hour == start_time.hour && (start_time.min <= meeting.end_time.min)
         errors.add(:start_time, "Meeting has already been schedule on this time!!")
       end
